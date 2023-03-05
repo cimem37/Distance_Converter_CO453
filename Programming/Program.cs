6a93f7f5a16f99bf44e63a0388276ddf763901fa
cimem37 <76502749+cimem37@users.noreply.github.com>
2023-03-05 04:43:20 +0000

    Header v1
    made a header testing how it looks ( one of the basic features )

--- Programming/Program.cs
+++ Programming/Program.cs
@@ -1,5 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Hello test test");
-Console.WriteLine("Hello , Line 1");
-Console.WriteLine("Hello , Line 2");
-Console.WriteLine("Hello , Line 3");
+Console.WriteLine("- - - - - - - - - - - - - - - - ");
+Console.WriteLine("   Bruno's distance converter   ");
+Console.WriteLine("- - - - - - - - - - - - - - - - ");
