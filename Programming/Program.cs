de731a9657a9ffae1c29e3f2ee0617584a5c9ae2
cimem37 <76502749+cimem37@users.noreply.github.com>
2023-03-05 07:03:55 +0000

    Added input method / started assigning unit
    The user will initially have to input his number regardless of the unit first then will be given a choice if successfully imputed number ( accepts only numerical values ) to select the measurement unit which is to assign ti the numerical value

--- Programming/Program.cs
+++ Programming/Program.cs
@@ -1,3 +1,40 @@
-Console.WriteLine("- - - - - - - - - - - - - - - - ");
-Console.WriteLine("   Bruno's distance converter   ");
-Console.WriteLine("- - - - - - - - - - - - - - - - ");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("- - - - - - - - - - - - - - - - ");
+        Console.WriteLine("   Bruno's distance converter   ");
+        Console.WriteLine("- - - - - - - - - - - - - - - - ");
+        
+        Console.WriteLine("Please enter the numerical amount : ");
+        double value = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please assign the measurement unit for the inputted number :");
+        Console.WriteLine("1. Miles");
+        Console.WriteLine("2. Feet");
+        Console.WriteLine("3. Meters");
+
+        int unit = int.Parse(Console.ReadLine());
+
+        switch (unit)
+        {
+            case 1:
+                Console.WriteLine("{0} miles", value);
+                break;
+            case 2:
+                Console.WriteLine("{0} feet", value);
+                break;
+            case 3:
+                Console.WriteLine("{0} meters", value);
+                break;
+            default:
+                Console.WriteLine("Invalid selection.");
+                break;
+        }
+        
+        Console.WriteLine("- - - - - - - - - - - - - - - - ");
+        Console.ReadLine();
+    }
+}
